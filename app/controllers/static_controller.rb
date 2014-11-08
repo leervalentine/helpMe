f618ae82f1ce29b84ac507ad3e9ffddaf6062e62
dushi27 <dushi27@gmail.com>
2014-11-08 23:08:59 +0000

    front end integrations

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,11 +2,6 @@ class StaticController < ApplicationController
   def home
   end
   
-  def voice
-    <?xml version="1.0" encoding="UTF-8"?>
-    <Response>
-        <Say voice="woman">This is Safe dot me. We just got you destress. Are you okay?</Say>
-        <Record maxLength="20" />
-    </Response>
+  def thankyou
   end
 end
